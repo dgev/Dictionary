af0c6fe5c1100ed3c2b0838a659e283cf4c914d4
dgev <49248374+dgev@users.noreply.github.com>
2019-11-03 23:18:20 +0400

    Dynamic Dictionary

--- Dictionary.java
+++ Dictionary.java
@@ -39,11 +39,28 @@ public class Dictionary<K, V> implements Map {
 		return objects;
 	}
 
+	private DictionaryObject<K, V> getObject(Object key) {
+		DictionaryObject<K, V> current = objects[keyToHash(key)];
+		if (current != null) {
+			if (current.getKey().equals(key)) {
+				return current;
+			}
+			current = current.getNext();
+			while (current != null) {
+				if (current.getKey().equals(key)) {
+					return current;
+				}
+				current = current.getNext();
+			}
+		}
+		return null;
+	}
+
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		for (DictionaryObject<K, V> obj : objects) {
-			if (objects.length > 0 && obj != null) {
+			if (size > 0 && obj != null) {
 				return false;
 			}
 		}
@@ -57,7 +74,8 @@ public class Dictionary<K, V> implements Map {
 	@Override
 	public boolean containsKey(Object key) {
 		// TODO Auto-generated method stub
-		if (objects[keyToHash(key)] != null) {
+		DictionaryObject<K, V> obj = getObject(key);
+		if (obj != null) {
 			return true;
 		}
 		return false;
@@ -67,8 +85,17 @@ public class Dictionary<K, V> implements Map {
 	public boolean containsValue(Object value) {
 		// TODO Auto-generated method stub
 		for (DictionaryObject<K, V> obj : objects) {
-			if (obj != null && obj.getValue().equals(value)) {
-				return true;
+			if (obj != null) {
+				if (obj.getValue().equals(value)) {
+					return true;
+				}
+				obj = obj.getNext();
+				while (obj != null) {
+					if (obj.getValue().equals(value)) {
+						return true;
+					}
+					obj = obj.getNext();
+				}
 			}
 		}
 		return false;
@@ -77,8 +104,9 @@ public class Dictionary<K, V> implements Map {
 	@Override
 	public Object get(Object key) {
 		// TODO Auto-generated method stub
-		if (objects[keyToHash(key)] != null) {
-			return objects[keyToHash(key)].getValue();
+		DictionaryObject<K, V> current = getObject(key);
+		if (current != null) {
+			return current.getValue();
 		}
 		return null;
 	}
@@ -86,23 +114,64 @@ public class Dictionary<K, V> implements Map {
 	@Override
 	public Object put(Object key, Object value) {
 		// TODO Auto-generated method stub
-		int current_key = keyToHash(key);
-		DictionaryObject<K, V> obj = new DictionaryObject(key, value, null);
-		objects[current_key] = obj;
-		size++;
-		return obj;
+		if (getObject(key) == null) {
+			int current_key = keyToHash(key);
+			DictionaryObject<K, V> obj = new DictionaryObject(key, value, null);
+			DictionaryObject<K, V> current = objects[current_key];
+			if (current == null) {
+				objects[current_key] = obj;
+			} else {
+				while (current.getNext() != null) {
+					current = current.getNext();
+				}
+				current.setNext(obj);
+			}
+			size++;
+			return obj;
+		}
+		return null;
 	}
 
 	@Override
 	public Object remove(Object key) {
 		// TODO Auto-generated method stub
-		for (DictionaryObject<K, V> obj : objects) {
-			if (obj != null && obj.getKey().equals(key)) {
-				V removed = obj.getValue();
-				obj.setKey(null);
-				obj.setValue(null);
-				return removed;
+
+		DictionaryObject<K, V> object = getObject(key);
+		if (object != null) {
+			size--;
+			V removed = object.getValue();
+			DictionaryObject<K, V> next = object.getNext();
+			if (object.equals(objects[keyToHash(key)])) {
+				if (next != null) {
+					DictionaryObject<K, V> last = next.getNext();
+					while (next.getNext() != null && last.getNext() != null) {
+						next = next.getNext();
+					}
+					if (last == null) {
+						object.setKey(next.getKey());
+						object.setValue(next.getValue());
+						object.setNext(null);
+					} else {
+						object.setKey(last.getKey());
+						object.setValue(last.getValue());
+						next.setNext(null);
+					}
+				}else {
+					objects[keyToHash(key)] = null;
+				}
+			} else {
+				DictionaryObject<K, V> previous = objects[keyToHash(key)];
+				while (!previous.getNext().equals(object)) {
+					previous = previous.getNext();
+				}
+				if (next == null) {
+					previous.setNext(null);
+				} else {
+					object.setNext(null);
+					previous.setNext(next);
+				}
 			}
+			return removed;
 		}
 		return null;
 	}
@@ -130,6 +199,11 @@ public class Dictionary<K, V> implements Map {
 		for (int i = 0; i < size; i++) {
 			if (objects[i] != null) {
 				array.add(objects[i].getKey());
+				DictionaryObject<K, V> next = objects[i].getNext();
+				while (next != null) {
+					array.add(next.getKey());
+					next = next.getNext();
+				}
 			}
 		}
 		return array;
@@ -142,6 +216,11 @@ public class Dictionary<K, V> implements Map {
 		for (DictionaryObject<K, V> obj : objects) {
 			if (obj != null) {
 				list.add(obj.getValue());
+				DictionaryObject<K, V> next = obj.getNext();
+				while (next != null) {
+					list.add(next.getValue());
+					next = next.getNext();
+				}
 			}
 		}
 		return list;
@@ -153,6 +232,11 @@ public class Dictionary<K, V> implements Map {
 		for (int i = 0; i < size; i++) {
 			if (objects[i] != null) {
 				set.add(objects[i]);
+				DictionaryObject<K, V> next = objects[i].getNext();
+				while (next != null) {
+					set.add(next);
+					next = next.getNext();
+				}
 			}
 		}
 		return set;
@@ -161,49 +245,64 @@ public class Dictionary<K, V> implements Map {
 	public static void main(String[] args) {
 
 		Dictionary<String, String> dict = new Dictionary<String, String>(6);
+		
+		System.out.println(dict.isEmpty());
 
-//        dict.put(1, "value");
+		dict.put("1", "value");
+		dict.put("1", "anotherValue");
+		dict.put("10", "value2");
+		dict.put("7", "value8");
 
-		dict.put("4", "value2");
-		dict.put("5", "value6");
-		dict.put("6", "value8");
-		Map<String, String> dicto = new HashMap<String, String>(6);
+//		System.out.println(dict.size());
 
-//      dict.put(1, "value");
+//		System.out.println(dict.get("1"));
+//		System.out.println(dict.get("7"));
 
-		dicto.put("1", "valuee");
-		dicto.put("2", "valueferf");
-		dicto.put("3", "valuefjyrg");
+//		System.out.println(dict.containsValue("value"));
 
-		Set set;
-		set = dict.keySet();
-		Iterator iterator = set.iterator();
-		while (iterator.hasNext()) {
-			String key = (String) iterator.next();
-			System.out.println(key);
-		}
-		System.out.println(set.iterator());
+//		Map<String, String> dicto = new HashMap<String, String>(6);
+//		dicto.put("1", "valuee");
+//		dicto.put("2", "valueferf");
+//		dicto.put("3", "valuefjyrg");
 
-		Set entry = dict.entrySet();
-		for (Object number : entry) {
-			System.out.println("Number = " + number);
-		}
+//		dict.putAll(dicto);
 
-		dict.putAll(dicto);
+//		dict.remove("7");
+//		System.out.println(dict.get("1"));
+
+//		DictionaryObject<String, String>[] obj = dict.getArr();
+//		for (int i = 0; i < obj.length; i++) {
+//			if (obj[i] != null) {
+//				System.out.println(obj[i].getValue());
+//				DictionaryObject<String, String> nexObject = obj[i].getNext();
+//				while (nexObject != null) {
+//					System.out.println(nexObject.getValue());
+//					nexObject = nexObject.getNext();
+//				}
+//			}
+//		}
+
+//		Set set;
+//		set = dict.keySet();
+//		Iterator iterator = set.iterator();
+//		while (iterator.hasNext()) {
+//			String key = (String) iterator.next();
+//			System.out.println(key);
+//		}
+
+//		Set entry = dict.entrySet();
+//		for (Object object : entry) {
+//			System.out.println("DictionaryObject = " + object);
+//		}
+
 //		System.out.println(dict.isEmpty());
 //		System.out.println(dict.size());
-		Collection arrlist = dict.values();
-		
-		for (Object number : arrlist) {
-	         System.out.println("Number = " + number);
-	      } 
 
-		DictionaryObject<String, String>[] obj = dict.getArr();
-		for (int i = 0; i < obj.length; i++) {
-			if (obj[i] != null) {
-				System.out.println(obj[i].getValue());
-			}
-		}
+//		Collection arrlist = dict.values();
+//
+//		for (Object object : arrlist) {
+//			System.out.println("Value is " + object);
+//		}
+
 	}
-
 }
